921c8257945e862dca498ea238b1ae0950ac43d6
Matthew Aguirre <matt.aguirre@gmail.com>
2017-01-27 22:50:40 -0500

    PathBuild is self-registering

--- src/main/java/com/artistech/ee/web/PathBuild.java
+++ src/main/java/com/artistech/ee/web/PathBuild.java
@@ -12,6 +12,7 @@ import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -24,6 +25,7 @@ import org.apache.commons.io.IOUtils;
  *
  * @author matta
  */
+@WebServlet(name = "PathBuild", urlPatterns = {"/PathBuild"})
 public class PathBuild extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
